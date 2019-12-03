0a2285861a0c0a8785cb5edc9aec54ce2d316eed
Tadej Matek <matektadej@gmail.com>
2019-12-03 13:22:13 +0100

    atom initial package.

---
+++ spec/handlebars-hyperclick-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HandlebarsHyperclickView from '../lib/handlebars-hyperclick-view';
+
+describe('HandlebarsHyperclickView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
